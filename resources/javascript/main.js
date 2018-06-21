fe1277c5f527745d02ec89c798a397353c2ff6d0
Chris Hardy <chrishardy@Chriss-MBP.home>
2018-06-22 00:09:12 +0100

    Added mobile menu." git push origin master exit d cd

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,1 +1,15 @@
+function main() {
 
+  $('.mobile').on('click',
+    function() {
+      $('.mobilenav').animate({'right': '0'});
+  });
+
+  $('body').click(function(e) {
+    if( $(e.target).closest(".mobile").length > 0 ) {
+      return false;
+  }
+      $('.mobilenav').animate({'right': '-150'});
+  });
+}
+$(document).ready(main);
