d4b15d414825082a98c15b51b87091716e912ade
Chris Hardy <chrishardy@Chriss-MBP.home>
2018-06-23 19:36:57 +0100

    edit mobile nav.

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -5,13 +5,12 @@ function main() {
       $('.mobilenav').animate({'right': '0'});
   });
 
-  $('body').on('click touchstart',function(e) {
+  $('#content').click(function(e) {
     if( $(e.target).closest(".mobile").length > 0 ) {
       return false;
   }
       $('.mobilenav').animate({'right': '-190'});
   });
-
 
 }
 $(document).ready(main);
